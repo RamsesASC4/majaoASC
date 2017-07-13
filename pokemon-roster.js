69582e217e48b57c49ef6ba86fc4433633d389f7
majaoASC4 <robloxphillz@gmail.com>
2017-07-13 08:55:02 -0700

    All bonuses so far completed during ASC SI

---
+++ pokemon-roster.js
@@ -0,0 +1,24 @@
+var charizard = ["Charizard", ["Type:", "Fire"], ["HP:", 200], ["ATK:", 120], ["DEF:", 150],["Legend:", false]];
+var pikachu = ["Pikachu", ["Type:", "Electric"], ["HP:",80], ["ATK:", 50], ["DEF:", 50],["Legend:", false]];
+var mew = ["Mew", ["Type:", "Psychic"], ["HP:", 100], ["ATK:", 120], ["DEF:", 150],["Legend:", true]];
+var umbra = ["Umbra", ["Type:", "Dark"], ["HP:", 120], ["ATK:", 120], ["DEF:", 150],["Legend:", false]];
+var dragonite = ["Dragonite", ["Type:", "Fire"], ["HP:", 200], ["ATK:", 120], ["DEF:", 150],["Legend:", false]];
+var mewtwo = ["Mewtwo", ["Type:", "Psychic"], ["HP:", 150], ["ATK:", 200], ["DEF:", 80],["Legend:", true]];
+
+var pokedex = [charizard,pikachu,mew,mewtwo,umbra,dragonite];
+
+for(var i=0;i<pokedex.length;i++){
+    var str = pokedex[i][0] + " > ";
+    for(var j=1;j<pokedex[i].length;j++){
+        var data = "";
+      for(var k=0;k<pokedex[i][j].length;k++){
+          data += pokedex[i][j][k] + " "
+      }
+      if(j == pokedex[i].length - 1){
+          str += data + " ";
+      } else {
+         str += data + " > ";
+      }
+    }
+    console.log(str)
+}
